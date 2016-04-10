a6cee4342d538cf7124a1acd7bc9ae96f06e52f1
Abhas Arya <abhasarya@Abhass-MacBook-Pro.local>
2016-04-10 00:42:15 -0400

    initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MentalHealthApp::Application.config.secret_key_base = '5e6c4b4f4374b6261ef4f915faed690cd4f1abc826dc515ae55eb4f5358ef386f6be15250f083d7e33ee24fec9b5e173153936fd5b4d4a513a4daa4cdf6ecf8e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MentalHealthApp::Application.config.session_store :cookie_store, key: '_mental-health-app_session'
